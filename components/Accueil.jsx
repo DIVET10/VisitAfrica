962e98dc2922f2bc5a00838cd1706c83f54ef38d
Paguem Tagang Michele Sephora <152933754+Mpag10@users.noreply.github.com>
2024-05-15 17:28:30 -0400

    Update Accueil.jsx
    correct a word

--- components/Accueil.jsx
+++ components/Accueil.jsx
@@ -3,7 +3,7 @@ import styles from './Accueil.module.css';
 export default function Accueil() {
     return (
         <div className={styles.welcome}>
-           Bienvenue sur notre site des meilleurs sites touristiques en Afrique !
+           Bienvenue dans notre site des meilleurs sites touristiques en Afrique !
         </div>
     );
 }
