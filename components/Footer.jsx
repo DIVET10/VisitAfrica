6d4acde38c90008b676332c2f879e86bf944645d
1Obed <obedluboy56@gmail.com>
2024-05-20 23:57:22 -0400

    Ceci est pour la page d'acceuil

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,10 +3,34 @@ import styles from './Footer.module.css';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div>&copy; VisitAfrica</div>
-            <div>Adresse: 123 Olmstar street , Ottawa, Canada</div>
-            <div>Téléphone: +123 456 789</div>
-            <div> Email: info@visitafrica.com</div>
+            <div className={styles.container}>
+                <div className="row">
+                    <div className="col-md-3">
+                        <h4>À propos de nous</h4>
+                        <p>Découvrez les merveilles de l'Afrique avec notre site touristique</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h4>Nos Destinations</h4>
+                        <ul className={styles.list}>
+                            <li><a href="#">Kenya</a></li>
+                            <li><a href="#">Afrique du Sud</a></li>
+                            <li><a href="#">Tanzanie</a></li>
+                            <li><a href="#">Découvrez plus ici</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3">
+                        <h4>Contactez-nous</h4>
+                        <ul className={styles.list}>
+                            <li><a href="mailto:contact@votresite.com">Email: visitafrica@africa.com</a></li>
+                            <li><a href="tel:+123456789">Téléphone: +123456789</a></li>
+                            <li>Adresse: 123 Rue de la Tour, Katina, Africa</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3 text-center">
+                        <p className="mt-3">&copy; 2024 VisitAfrica. Tous droits réservés.</p>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
--- components/MenuNav.jsx
+++ components/MenuNav.jsx
@@ -4,12 +4,12 @@ export default function MenuNav() {
     return (
         <nav className={styles.nav}>
             <ul>
-                <li><a href="#">Accueil</a></li>
-                <li><a href="#">Safaris</a></li>
-                <li><a href="#">Plages</a></li>
-                <li><a href="#">Sites historiques</a></li>
+                {/* <li><a href="#">Accueil</a></li>
+               
                 <li><a href="#">Destinations populaires</a></li>
-                <li><a href="#">Contact</a></li>
+
+
+                <li className={styles.contact}><a href="#">Contact</a></li> */}
             </ul>
         </nav>
     );
