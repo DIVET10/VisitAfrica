0d341ef674117b03a045d939088c05072e4b1b01
Paguem Tagang Michele Sephora <152933754+Mpag10@users.noreply.github.com>
2024-06-09 21:45:33 -0400

    loading barre
    barre de chargement

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,31 @@
-'use client'
-import { useState } from 'react';
+'use client';
+
+import React from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import styles from './layout.module.css';
-import "./globals.css";
+import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from 'next/font/google';
+import LoadingBar from '@/components/LoadingBar';
+
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
-      <body className={inter.className + ' ' + styles.body}>
-        <div>
-          <Header/>
-        
-          <main className={styles.main}>
-           {children}
-          </main>
-          <Footer />
-        </div>
+      <body className={`${inter.className} ${styles.body}`}>
+       
+          <div>
+            <Header />
+            <LoadingBar/>
+            <main className={styles.main}>
+              {children}
+            </main>
+            <Footer />
+          </div>
+       
       </body>
     </html>
   );
